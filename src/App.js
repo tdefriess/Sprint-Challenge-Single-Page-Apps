54f43c41b9bcc8b19c5f2f6accd7172200f4cea4
Tyler de Friess <tyler.defriess@gmail.com>
2020-01-31 11:26:17 -0500

    MVP task: add router, use styling complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Header from "./components/Header.js";
+import WelcomePage from "./components/WelcomePage";
+import CharacterList from "./components/CharacterList";
+import LocationsList from "./components/LocationsList";
+import { Route } from "react-router-dom";
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Route exact path="/" component={WelcomePage} />
+      <Route path="/characters" component={CharacterList} />
+      <Route path="/locations" component={LocationsList} />
     </main>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 export default function Header() {
+  const StyledLink = styled(Link)`
+    color: black;
+    text-decoration: none;
+    &:hover {
+      color: #a9d3e9;
+    }
+  `;
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <StyledLink to="/">Home</StyledLink>
+      <StyledLink to="/characters">Characters</StyledLink>
+      {/* <Link to="/locations">Locations</Link> */}
     </header>
   );
 }
